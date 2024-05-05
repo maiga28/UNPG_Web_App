107434ac8be82469f3d3fd2f0ec970b368c39d04
maiga28 <sidimaiga.work@gmail.com>
2024-05-05 15:21:23 +0000

    templating format

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1!a6p+q)jmni(ajzy9tm-e$@5gjy#6k#(%d8tq5q#$nwg6pkcs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['unpg.pythonanywhere.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
